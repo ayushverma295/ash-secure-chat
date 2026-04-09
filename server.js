e67b377f91e590a2df9689579f2df5beda5886d2
Ayush Kumar Verma <ayushverma29005@gmail.com>
2026-04-09 23:12:42 +0530

    Streamline server.js for better structure

--- server.js
+++ server.js
@@ -174,7 +174,6 @@ window.onload = function(){
 
 </script>
 
-
 <div id="exitScreen" style="display:none;position:fixed;top:0;left:0;width:100%;height:100%;background:#0f172a;color:white;justify-content:center;align-items:center;flex-direction:column;">
   <h1>👋 You left the room</h1>
   <p>Made in Bihar, With 🤍 Ayush Verma</p>
